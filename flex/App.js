6a5d66b5eba42e15e67d704a7906a31a76ee73ea
nathan2m <nathan.manera@gmail.com>
2021-08-31 20:12:58 -0300

    Project: FlexBox no React Native

---
+++ flex/App.js
@@ -0,0 +1,56 @@
+//import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import {
+    StatusBar,
+    StyleSheet,
+    Text,
+    View,
+} from 'react-native';
+
+export default function App() {
+    return (
+        <View style={styles.container}>
+            <StatusBar></StatusBar>
+            {/* <View style={styles.box} />
+            <View style={styles.box} />
+            <View style={styles.box} />
+            <View style={styles.box} />
+            <View style={styles.box} /> */}
+            <View style={styles.header}>
+                <Text>Voltar</Text>
+                <Text>Titulo</Text>
+                <Text>Perfil</Text>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#CCC',
+        /* flexDirection: 'row',
+        flexWrap: 'wrap',
+        alignContent: 'space-between', */
+    },
+
+    header: {
+        height: 60,
+        backgroundColor: '#fff',
+        //paddingTop: 20,
+        paddingHorizontal: 20,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
+
+    /* box: {
+        flex: 1,
+        minWidth: 100,
+        height: 100,
+        backgroundColor: '#666',
+        borderColor: '#999',
+        borderWidth: 5,
+        margin: 10,
+    }, */
+});
---
+++ flex/README.md
@@ -0,0 +1,17 @@
+# FlexBox in React Native
+
+React Native project created from [this](https://www.youtube.com/watch?v=dUkkOdhyO1w) video.
+
+## Used technologies:
+- JavaScript ES6
+- [React Native](https://reactnative.dev/)
+- [Expo](https://expo.dev/)
+
+## How to run?
+- Clone this repository.
+- Inside `/flex` folder, run `yarn install` to install dependencies.
+- Run `expo start` to run the app in the expo development mode.
+- Open [http://localhost:19002/](http://localhost:19002/) to view it in the browser.
+- Select your device (Android or iOS) or an emulator to run the app.
+    - The app will reload if you make edits.
+    - You will also see any lint errors in the console.
