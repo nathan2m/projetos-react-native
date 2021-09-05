50b7dfff4b1ce111490386be1db28acc28fd6906
nathan2m <nathan.manera@gmail.com>
2021-09-05 15:50:11 -0300

    Project: FlexBox no React Native
    Update README.md and 'comandos utilizados.txt'

--- flex/App.js
+++ flex/App.js
@@ -1,56 +1,50 @@
-//import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import {
-    StatusBar,
-    StyleSheet,
-    Text,
-    View,
-} from 'react-native';
+import {StatusBar, StyleSheet, Text, View} from 'react-native';
 
 export default function App() {
-    return (
-        <View style={styles.container}>
-            <StatusBar></StatusBar>
-            {/* <View style={styles.box} />
-            <View style={styles.box} />
-            <View style={styles.box} />
-            <View style={styles.box} />
-            <View style={styles.box} /> */}
-            <View style={styles.header}>
-                <Text>Voltar</Text>
-                <Text>Titulo</Text>
-                <Text>Perfil</Text>
-            </View>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <StatusBar />
+      {/* <View style={styles.box} />
+        <View style={styles.box} />
+        <View style={styles.box} />
+        <View style={styles.box} />
+        <View style={styles.box} /> */}
+      <View style={styles.header}>
+        <Text>Voltar</Text>
+        <Text>Titulo</Text>
+        <Text>Perfil</Text>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#CCC',
-        /* flexDirection: 'row',
-        flexWrap: 'wrap',
-        alignContent: 'space-between', */
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#CCC',
+    /* flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignContent: 'space-between', */
+  },
 
-    header: {
-        height: 60,
-        backgroundColor: '#fff',
-        //paddingTop: 20,
-        paddingHorizontal: 20,
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-    },
+  header: {
+    height: 60,
+    backgroundColor: '#fff',
+    //paddingTop: 20,
+    paddingHorizontal: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
 
-    /* box: {
-        flex: 1,
-        minWidth: 100,
-        height: 100,
-        backgroundColor: '#666',
-        borderColor: '#999',
-        borderWidth: 5,
-        margin: 10,
-    }, */
+  /* box: {
+    flex: 1,
+    minWidth: 100,
+    height: 100,
+    backgroundColor: '#666',
+    borderColor: '#999',
+    borderWidth: 5,
+    margin: 10,
+  }, */
 });
--- flex/README.md
+++ flex/README.md
@@ -5,13 +5,10 @@ React Native project created from [this](https://www.youtube.com/watch?v=dUkkOdh
 ## Used technologies:
 - JavaScript ES6
 - [React Native](https://reactnative.dev/)
-- [Expo](https://expo.dev/)
 
 ## How to run?
 - Clone this repository.
 - Inside `/flex` folder, run `yarn install` to install dependencies.
-- Run `expo start` to run the app in the expo development mode.
-- Open [http://localhost:19002/](http://localhost:19002/) to view it in the browser.
-- Select your device (Android or iOS) or an emulator to run the app.
-    - The app will reload if you make edits.
-    - You will also see any lint errors in the console.
+- Make sure you have a device (Android or iOS) connected or an emulator to run the app.
+- Run `npx react-native run-android` to run the app in android device or emulator.
+- Run `npx react-native run-ios` to run the app in iOS device or emulator.
