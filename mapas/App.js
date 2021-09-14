e35587f8d69838f876556544d329aaa9d8f52121
nathan2m <nathan.manera@gmail.com>
2021-09-13 22:27:15 -0300

    Project: Mapas no React Native

---
+++ mapas/App.js
@@ -0,0 +1,150 @@
+import React, {Component} from 'react';
+import {ScrollView, View, Text, StyleSheet, Dimensions} from 'react-native';
+
+import MapView from 'react-native-maps';
+
+export default class App extends Component {
+  state = {
+    places: [
+      {
+        id: 1,
+        title: 'Casa do café',
+        description: 'Café quentinho...',
+        latitude: -27.210671,
+        longitude: -49.63627,
+      },
+      {
+        id: 2,
+        title: 'RocketSeat',
+        description: 'Programação, empreendedorismo e mindset',
+        latitude: -27.200671,
+        longitude: -49.63627,
+      },
+      {
+        id: 3,
+        title: 'Casa do José',
+        description: 'José, tá em casa?',
+        latitude: -27.200671,
+        longitude: -49.62627,
+      },
+    ],
+  };
+
+  _mapReady = () => {
+    this.state.places[0].mark.showCallout();
+  };
+
+  render() {
+    const {latitude, longitude} = this.state.places[0];
+
+    return (
+      <View style={styles.container}>
+        <MapView
+          ref={map => (this.mapView = map)}
+          initialRegion={{
+            latitude,
+            longitude,
+            latitudeDelta: 0.0142,
+            longitudeDelta: 0.0231,
+          }}
+          style={styles.mapView}
+          rotateEnabled={false}
+          scrollEnabled={false}
+          zoomEnabled={false}
+          showsPointsOfInterest={false}
+          showBuildings={false}
+          onMapReady={this._mapReady}>
+          {this.state.places.map(place => (
+            <MapView.Marker
+              ref={mark => (place.mark = mark)}
+              title={place.title}
+              description={place.description}
+              key={place.id}
+              coordinate={{
+                latitude: place.latitude,
+                longitude: place.longitude,
+              }}
+            />
+          ))}
+        </MapView>
+        <ScrollView
+          style={styles.placesContainer}
+          horizontal
+          pagingEnabled
+          showsHorizontalScrollIndicator={false}
+          onMomentumScrollEnd={e => {
+            const place =
+              e.nativeEvent.contentOffset.x > 0
+                ? e.nativeEvent.contentOffset.x / Dimensions.get('window').width
+                : 0;
+
+            const {latitude, longitude, mark} = this.state.places[place];
+
+            this.mapView.animateToCoordinate(
+              {
+                latitude,
+                longitude,
+              },
+              500,
+            );
+
+            setTimeout(() => {
+              mark.showCallout();
+            }, 500);
+          }}>
+          {this.state.places.map(place => (
+            <View key={place.id} style={styles.place}>
+              <Text style={styles.title}>{place.title}</Text>
+              <Text style={styles.description}>{place.description}</Text>
+            </View>
+          ))}
+        </ScrollView>
+      </View>
+    );
+  }
+}
+
+const {height, width} = Dimensions.get('window');
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end',
+  },
+
+  mapView: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+  },
+
+  placesContainer: {
+    width: '100%',
+    maxHeight: 200,
+  },
+
+  place: {
+    width: width - 40,
+    maxHeight: 200,
+    backgroundColor: '#FFF',
+    marginHorizontal: 20,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    padding: 20,
+  },
+
+  title: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    backgroundColor: 'transparent',
+  },
+
+  description: {
+    color: '#999',
+    fontSize: 12,
+    marginTop: 5,
+  },
+});
---
+++ mapas/README.md
@@ -0,0 +1,14 @@
+# Maps in React Native
+
+React Native project created from [this](https://www.youtube.com/watch?v=2takPFib5uE) video. Original project repository [here](https://github.com/rocketseat-content/youtube-codequinta-mapas-react-native).
+
+## Used technologies:
+- JavaScript ES6
+- [React Native](https://reactnative.dev/)
+
+## How to run?
+- Clone this repository.
+- Inside `/mapas` folder, run `yarn install` to install dependencies.
+- Make sure you have a device (Android or iOS) connected or an emulator to run the app.
+- Run `npx react-native run-android` to run the app in android device or emulator.
+- Run `npx react-native run-ios` to run the app in iOS device or emulator.
